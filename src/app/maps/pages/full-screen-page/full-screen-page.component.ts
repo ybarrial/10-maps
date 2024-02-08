ddd576b145fccfbd3678a6b814a76484a4c313e7
Your Name <yefferson.barrial@avivel.com.pe>
2024-02-07 19:21:40 -0500

    addc implement map in html

--- src/app/maps/pages/full-screen-page/full-screen-page.component.ts
+++ src/app/maps/pages/full-screen-page/full-screen-page.component.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
+
+import { AfterViewInit, Component } from '@angular/core';
+
+(mapboxgl as any).accessToken = 'pk.eyJ1IjoieWJhcnJpYWxhIiwiYSI6ImNsc2Nkd2NibjBtemkybHJrZjJhZ2czc2EifQ.76Aa5iuvgAs7FseLM4Lw_A';
 
 @Component({
   templateUrl: './full-screen-page.component.html',
   styleUrls: ['./full-screen-page.component.css']
 })
-export class FullScreenPageComponent {
+export class FullScreenPageComponent implements AfterViewInit {
+
+  ngAfterViewInit(): void {
+    const map = new mapboxgl.Map({
+      container: 'map', // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: [-74.5, 40], // starting position [lng, lat]
+      zoom: 9, // starting zoom
+    });
+  }
 
 }
