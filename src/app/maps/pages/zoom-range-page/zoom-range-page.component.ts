5fa1b970203fa6146e32732bf46845ca712199a0
Your Name <yefferson.barrial@avivel.com.pe>
2024-02-08 18:25:36 -0500

    reference element #map to div

--- src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
+++ src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
@@ -1,9 +1,29 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { Map } from 'mapbox-gl'
+
 
 @Component({
   templateUrl: './zoom-range-page.component.html',
   styleUrls: ['./zoom-range-page.component.css']
 })
-export class ZoomRangePageComponent {
+export class ZoomRangePageComponent implements AfterViewInit {
+
+  @ViewChild('map') divMap?: ElementRef;
+
+
+  ngAfterViewInit(): void {
+
+    console.log(this.divMap);
+
+    if (!this.divMap) throw 'El elemento HTML no fue encontrado';
+
+    const map = new Map({
+      //container: 'map', // container ID
+      container: this.divMap.nativeElement, // referencia del elemento
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: [-74.5, 40], // starting position [lng, lat]
+      zoom: 9, // starting zoom
+    });
+  }
 
 }
