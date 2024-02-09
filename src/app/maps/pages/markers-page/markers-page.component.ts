3dec07ddda67802587c86ca5b521ed792b816ccf
Your Name <yefferson.barrial@avivel.com.pe>
2024-02-08 21:52:46 -0500

    add markers dinamically

--- src/app/maps/pages/markers-page/markers-page.component.ts
+++ src/app/maps/pages/markers-page/markers-page.component.ts
@@ -39,4 +39,26 @@ export class MarkersPageComponent implements AfterViewInit {
 
   }
 
+  createMarker() {
+    if ( !this.map )  return;
+
+    const color = '#xxxxxx'.replace(/x/g, y=>(Math.random()*16|0).toString(16));
+    const lngLat = this.map.getCenter();
+
+
+    this.addMarker(lngLat, color)
+  }
+
+  addMarker( lngLat: LngLat, color: string ) {
+    if ( !this.map ) return;
+
+    const marker = new Marker({
+      color: color,
+      draggable: true
+    })
+      .setLngLat( lngLat )
+      .addTo( this.map );
+  }
+
+
 }
