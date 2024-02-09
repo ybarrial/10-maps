5d8ba00df4f6d786f79b74657800229431983fde
Your Name <yefferson.barrial@avivel.com.pe>
2024-02-08 20:10:05 -0500

    control zoom level

--- src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
+++ src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
@@ -10,20 +10,52 @@ export class ZoomRangePageComponent implements AfterViewInit {
 
   @ViewChild('map') divMap?: ElementRef;
 
+  public zoom: number = 10;
+  public map?: Map;
+
 
   ngAfterViewInit(): void {
-
     console.log(this.divMap);
 
     if (!this.divMap) throw 'El elemento HTML no fue encontrado';
 
-    const map = new Map({
+    this.map = new Map({
       //container: 'map', // container ID
       container: this.divMap.nativeElement, // referencia del elemento
       style: 'mapbox://styles/mapbox/streets-v12', // style URL
       center: [-74.5, 40], // starting position [lng, lat]
-      zoom: 9, // starting zoom
+      zoom: this.zoom, // starting zoom
     });
+
+    this.mapListeners();
+  }
+
+  mapListeners() {
+    if (!this.map) throw 'Mapa no inicializado';
+
+    this.map.on("zoom", (ev) => {
+      console.log(ev);
+      this.zoom = this.map!.getZoom();
+    })
+
+    this.map.on("zoomend", (ev) => {
+      console.log(ev);
+      if( this.map!.getZoom() < 18 ) return;
+      this.map!.zoomTo(18);
+    })
+  }
+
+  zoomIn() {
+    this.map?.zoomIn();
+  }
+
+  zoomOut() {
+    this.map?.zoomOut();
+  }
+
+  zoomChanged(value: string) {
+    this.zoom = Number(value);
+    this.map?.zoomTo(this.zoom);
   }
 
 }
