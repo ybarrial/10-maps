b833287a647882106c32544141570618921969d9
Your Name <yefferson.barrial@avivel.com.pe>
2024-02-10 13:09:53 -0500

    add map to div styles,

--- src/app/maps/components/mini-map/mini-map.component.ts
+++ src/app/maps/components/mini-map/mini-map.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { Map, Marker } from 'mapbox-gl';
 
 @Component({
-  selector: 'app-mini-map',
+  selector: 'map-mini-map',
   templateUrl: './mini-map.component.html',
   styleUrls: ['./mini-map.component.css']
 })
-export class MiniMapComponent {
+export class MiniMapComponent implements AfterViewInit {
+
+  public map?: Map;
+
+  public zoom: number = 15;
+
+  @Input() lngLat?: [number, number];
+
+  @ViewChild('map') divMap?: ElementRef;
+
+
+  ngAfterViewInit(): void {
+    if ( !this.divMap?.nativeElement) throw "Map Div not found";
+    if( !this.lngLat ) throw "LngLat can't be null";
+
+
+    this.map = new Map({
+      //container: 'map', // container ID
+      container: this.divMap.nativeElement, // referencia del elemento
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: this.lngLat, // starting position [lng, lat]
+      zoom: this.zoom, // starting zoom
+      interactive: false, // no move no nothing.
+    });
+
+    new Marker()
+      .setLngLat(this.lngLat)
+      .addTo( this.map )
+
+  }  
 
 }
